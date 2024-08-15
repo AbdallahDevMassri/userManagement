50f044bd4c0ae515d488964e0bc781fd25c0cb35
Abdallah Massri <a.v.e@live.com>
2024-08-15 13:52:19 +0300

    first commit

---
+++ app/src/main/java/com/example/usermanagementapp/ApiInterface.java
@@ -0,0 +1,9 @@
+package com.example.usermanagementapp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiInterface {
+    @GET("posts/1")
+    public Call<Post> getPost();
+}
